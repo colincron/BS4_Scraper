e66a0d92c04e87003c4707c577931c5585aada27
Colin <colint.cron@gmail.com>
2024-03-20 19:04:20 -0400

    added duplicate prevention in urlList. now outputs to file urls.txt

--- scraper.py
+++ scraper.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -7,21 +9,26 @@ urlList = [startUrl,]
 
 while len(urlList) > 0: 
     url = urlList[i]
-    print(url)
-    print("Length of urlList: " + str(len(urlList)))
+    #print(url)
+    print("Length of urlList: " + str(len(urlList)) + "\n")
+    print("Now scanning: " + url)
     i = i + 1
     
-    response = requests.get(url, headers = headers)
+    response = requests.get(url)
     htmlData = response.content
-    parsedData = BeautifulSoup(htmlData, "lxml") #lxml is fast and lenient
-    anchors = parsedData.find_all("a")
+    parsedData = BeautifulSoup(htmlData, "html.parser") #lxml is fast and lenient
+    #anchors = parsedData.find_all("a")
+    anchors = parsedData.find_all(lambda tag: tag.name == 'a' and tag.get('href'))
     
+    file = open('urls.txt','a')
     for a in anchors:
+        #print("A")
         #print(a)
         references = [a["href"]]
         for r in references:
-            if r.startswith("http"):
-                #print(r)
+            if r.startswith("http") and r not in urlList:
+                #print("R")
+                print(r + "\n")
                 urlList.append(r)
-                set(urlList) #order of list elements possibly lost by conversion, but duplicates removed
-                list(urlList)
+                file.write(r+"\n")
+                
