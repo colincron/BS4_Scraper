2424a88d0a6e9d6f772880b17350a1fa4929d80d
Colin <colint.cron@gmail.com>
2024-03-20 17:33:01 -0400

    test

--- scraper.py
+++ scraper.py
@@ -1,8 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
+startUrl = input("Start URL: ")
 i = 0
-urlList = ["https://www.kdnuggets.com/2023/04/stepbystep-guide-web-scraping-python-beautiful-soup.html/",]
+urlList = [startUrl,]
 
 while len(urlList) > 0: 
     url = urlList[i]
@@ -10,22 +11,17 @@ while len(urlList) > 0:
     print("Length of urlList: " + str(len(urlList)))
     i = i + 1
     
-    response = requests.get(url)
+    response = requests.get(url, headers = headers)
     htmlData = response.content
     parsedData = BeautifulSoup(htmlData, "lxml") #lxml is fast and lenient
-    #print(parsedData.prettify())
     anchors = parsedData.find_all("a")
     
     for a in anchors:
         #print(a)
         references = [a["href"]]
         for r in references:
-            if r.startswith("http") is True:
+            if r.startswith("http"):
                 #print(r)
                 urlList.append(r)
-                set(urlList) #order of list elements lost by conversion, but duplicates removed
+                set(urlList) #order of list elements possibly lost by conversion, but duplicates removed
                 list(urlList)
-    
-    if i > 5:
-        print(urlList)
-        break
