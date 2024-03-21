20df9e9164a1928161f002ebf4818b44c68975c5
Colin <colint.cron@gmail.com>
2024-03-20 22:51:10 -0400

    removed gratuitous print functions

--- dbfunctions.py
+++ dbfunctions.py
@@ -8,19 +8,12 @@ def writeToDB(urlToSave):
                             password = pword,
                             port = 5432)
     cur = conn.cursor()
-    print("\nDB CONNECTED\n")
     urlToSave = str(urlToSave)
-    print("1")
     sql = "INSERT INTO Websites (URL) VALUES ('{}');".format(urlToSave)
-    print("2")
     try:
-        print("3")
         with  conn.cursor() as cur:
-            print("4")
             cur.execute(sql, ("Websites"))
-            print("5")
             conn.commit()
-            print("Mighta worked!")
     except (Exception, psycopg2.DatabaseError) as error:
         cur.close()
         conn.close()
