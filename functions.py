cacb44628a91704ff333afe733a0d30f6e43d2ed
Colin <colint.cron@gmail.com>
2024-03-21 20:25:08 -0400

    removed port scanner

--- functions.py
+++ functions.py
@@ -12,31 +12,3 @@ def onionHandler(url):
         print("\nOnion detected\n")
         writeToDB(url, "onions", "url")
         return url
-    
-def domainScout(domain):
-    target = getIP(stripURL(domain))
-
-    print("Scanning Target: " + target)
-    print("Scanning started at:" + str(datetime.now()))
-  
-    try: 
-        for port in range(1,1024):
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            socket.setdefaulttimeout(.25)     
-            # returns error indicator
-            result = s.connect_ex((target,port))
-            print(result)
-            if result ==0:
-                print("Port {} is open".format(port))
-            s.close()
-         
-    except KeyboardInterrupt:
-        print("\n Exiting Program")
-        sys.exit()
-    except socket.gaierror:
-        print("\n Hostname Could Not Be Resolved")
-        sys.exit()
-    except socket.error:
-        print("\n Server not responding")
-        sys.exit()
-    return 0
