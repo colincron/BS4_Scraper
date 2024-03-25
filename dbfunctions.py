8fe944d424324ec2b70fcf6a48b90fc786d3b543
Colin <colint.cron@gmail.com>
2024-03-24 20:24:18 -0400

    moved function from dbfunctions.py to functions.py and deleted dbfunctions.py

--- dbfunctions.py
+++ dbfunctions.py
@@ -1,27 +1,7 @@
-import psycopg2
-from config import secret
+
 
 
 #createDB()
 
-def writeToDB(urlToSave, table, column):
-    conn = psycopg2.connect(database = "ScrapeDB",
-                            user="postgres",
-                            host="localhost",
-                            password = secret,
-                            port = 5432)
-    cur = conn.cursor()
-    urlToSave = str(urlToSave)
-    sql = "INSERT INTO {} ({}) VALUES ('{}');".format(table, column, urlToSave)
-    try:
-        with  conn.cursor() as cur:
-            cur.execute(sql, (table))
-            conn.commit()
-    except (Exception, psycopg2.DatabaseError) as error:
-        cur.close()
-        conn.close()
-        print(error)
-    cur.close()
-    conn.close()
-    return 0
+
 
